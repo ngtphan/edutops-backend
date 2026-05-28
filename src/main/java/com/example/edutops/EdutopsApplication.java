18c7830ce23b1414c14afbc83f863a23effe1eb0
Phan Ngọc Tài <ngoctaiphan.cv@gmail.com>
2026-05-28 14:13:06 +0700

    feat(init): initialize project structure and build configuration

---
+++ src/main/java/com/example/edutops/EdutopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.edutops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutopsApplication.class, args);
+	}
+
+}
